2862b975d7ad66fdf37f72f6dfab4398dc8d2c51
LuongMinhTien <193111>
2022-08-02 23:05:44 +0700

    change database link

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const connect = async () => {
   mongoose
     .connect(
+      `mongodb+srv://n18dcat069-shopee:n18dcat069-shopee@cluster0.rpxyi.mongodb.net/shopee`
     //   `mongodb+srv://<user>:<password>@cluster0.rpxyi.mongodb.net/<Tên database>`
-    `mongodb://localhost:27017/shopee`
+    // `mongodb://localhost:27017/shopee`
     )
     .then(() => {
       console.log("Ket noi database thanh cong");
